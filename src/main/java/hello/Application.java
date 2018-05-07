a9ebf1b5d9db06c8ae6307d710b925783bd594fc
kenneth.cootauco2@loblaw.ca <kenneth.cootauco2@loblaw.ca>
2018-05-07 08:52:17 -0400

    update context root for app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-    @RequestMapping("/")
+    @RequestMapping("/bootdocker")
     public String home() {
         return "Hello Docker World";
     }
